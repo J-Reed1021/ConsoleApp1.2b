3eccd28298218536ae3bbc9581397b928a52df9d
joshu <joshu@REEDPC>
2024-05-16 14:56:56 -0500

    Add project files.

---
+++ ConsoleApp1.2b/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NaturalNumbers1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+            int num4 = 4;
+            int num5 = 5;
+            int num6 = 6;
+            int num7 = 7;
+            int num8 = 8;
+            int num9 = 9;
+            int num10 = 10;
+
+            int sum = 0;
+            for (int i = 1; i == num1; i ++)
+          
+            { 
+            
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
